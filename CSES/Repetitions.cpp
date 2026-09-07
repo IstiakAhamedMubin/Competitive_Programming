8565282e6a1c8b2928e781b21f92bd410682208d
Istiak Ahamed <istiakop800@gmail.com>
2026-09-07 17:01:43 +0600

    Add solution for CSES problem Repetitions

---
+++ CSES/Repetitions.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    string s; 
+    cin>>s;
+    int current = 1;
+    int maximum = 1;
+    for(int i = 0; i<s.size()-1; i++) {
+        if (s[i] == s[i+1]) {
+            current ++;
+            if(current>maximum){
+            maximum = current;
+        }
+    }
+        else{
+            current = 1;
+           
+        }
+    }
+    cout<< maximum;
+}
