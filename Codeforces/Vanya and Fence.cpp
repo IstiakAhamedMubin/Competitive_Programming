c4aa41f980aa4b757ad778d60df2cd5dab130fa3
Istiak Ahamed <istiakop800@gmail.com>
2026-09-08 10:37:49 +0600

    Add solution for Codeforces problem Vanya and Fence

---
+++ Codeforces/Vanya and Fence.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    int n,h,nSonkhokBondurHight,width=0;
+    cin>>n>>h;
+    
+    for(int i = 0; i<n; i++) {
+        cin>>nSonkhokBondurHight;
+        if(nSonkhokBondurHight <= h) {
+            width+= 1;
+        }
+        else{
+            width+=2;
+        }
+    }
+    cout<<width<<endl;
+    return 0;
+}
